4331a12d3c70cae7f6dddb62813be849e388d9a4
ny <mac@nythepegas.us>
2025-07-28 08:58:15 -0400

    fix: windows builds by removing these

--- PyDunk/launch.py
+++ PyDunk/launch.py
@@ -11,10 +11,6 @@
 
 # nuitka-project-if: {OS} == "Windows":
 #    nuitka-project: --output-filename=PyDunk-windows-x86_64.exe
-#    nuitka-project: --include-module=jinxed.terminfo.vtwin10
-#    nuitka-project: --include-module=jinxed.terminfo.ansicon
-#    nuitka-project: --include-module=jinxed.terminfo.xterm
-#    nuitka-project: --include-module=jinxed.terminfo.xterm_256color
 #    nuitka-project: --user-package-configuration-file=pydunk-nuitka-package.config.yml
 # nuitka-project-if: {OS} == "Linux":
 #    nuitka-project: --output-filename=PyDunk-linux-x86_64.bin
